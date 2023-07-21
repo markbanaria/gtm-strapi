dcf734bee484522cb18921776e93657d8c27a269
Jamaico.Terrible <70144784+JamaicoXX@users.noreply.github.com>
2023-07-21 15:42:16 +0800

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,6 +9,9 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        },
         //ssl: {
         //  ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
         //},
