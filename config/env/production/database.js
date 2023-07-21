ab2187d46bf0acc6834ac893d5651a98d8d02f51
JamaicoXX <terrible.jamaico.me@gmail.com>
2023-07-21 16:25:07 +0800

    commit changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
+
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 module.exports = ({ env }) => ({
+  connection: {  
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
-        },
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
       },
-      debug: false,
     },
-  });
+      debug: false,
+  },
+});
